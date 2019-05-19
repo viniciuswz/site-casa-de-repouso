e02bf4713edfce6e8558c48953edc7d361a874c0
Daniel Costa <danielcost0705@gmail.com>
2019-05-19 04:01:19 -0300

    Construcao da model generica

--- Model/GenericaM.class.php
+++ Model/GenericaM.class.php
@@ -1,39 +1,75 @@
 <?php 
-
 namespace Model;
-use Db\DbConnection;
-
-class GenericaM extends DbConnection{    
+class GenericaM{    
     
-    private $dados = array();
+    private $dadosAtributos;    
 
-    public function __call($jaca, $jaquinha = array()){        
+    public function __call($jaca, $dados){   
+        $indValido = array(); // indicador se o dados que ira ser inserido esta valido     
         $acao = strtolower(substr($jaca,0,3)); //ficar tudo minusculo
         $nomeMetodo = strtolower(substr($jaca,3,strlen($jaca)));// ficar tudo minusculo    
         
         switch($acao){
             case 'set': 
-                foreach($jaquinha as $chaves => $valores){
-                    foreach($valores as $chave => $valor){                        
-                        $this->dados[$nomeMetodo][$chave] = $valor;
-                    }                                        
+                if(!is_array($dados)){ // é necessário que seja uma array
+                    throw new \Exception("É necessário setar os dados como Array para a propiedade $nomeMetodo", 720);                    
                 } 
+                $dados = $dados[0]; // nao preciso q fique no indice zero
+                if(!isset($dados['vlr'])){ // tem q existir o dado pra setar
+                    throw new \Exception("É necessário setar um valor na propiedade $nomeMetodo", 720);
+                }                
+                
+                if(isset($dados['validacoes'])){ // se precisar de validacoes
+                    if(!is_array($dados['validacoes'])){ // tem q estar em uma array
+                        throw new \Exception("É necessário setar as validacoes como Array para a propiedade $nomeMetodo", 720);
+                    }
+                    foreach($dados['validacoes'] as $vlr){ // percorrer a array
+                        switch(strtolower($vlr)){ // transformar em minusculo
+                            case 'not null': // nao pode ser vazio 
+                                // fix = nome que inventei q se refere a validacoes
+                                $indValido[] = $this->{"fix" . str_replace(" ", "", $vlr)}(["vlr" => $dados['vlr']]); // chama funcao dinamicamente
+                                // coloca o valor retornado pela validacao
+                                // false = invalidos
+                                // true = validos
+                                break;
+                            // para criar uma nova validacaoes é só criar um novo case, com o nome e depois copiar a linha de cima 
+                            // receita de bolo
+                            // depois tem q ir no switch case do fix é criar a validacao
+                        }    
+                    }                    
+                    foreach($indValido as $indValidoItem){ // percorrer a array com o resultado das validacoes
+                        if($indValidoItem == false){ // se for falso para a execucao
+                            throw new \Exception("Os dados setados da propiedade $nomeMetodo não são válidos", 720);                            
+                        }
+                    }                    
+                    $this->dadosAtributos[$nomeMetodo] = $dados['vlr']; // so depois de tudo seta o vlr
+                }else{ // nao quero nenhum validacao
+                    $this->dadosAtributos[$nomeMetodo] = $dados['vlr'];
+                }                        
                 break;
-            case 'get':
-                $data = array();
-                foreach($this->dados as $chaves => $valores){
-                    foreach($valores as $chave => $valor){
-                        $data[] = $this->dados[$chaves][$chave][$nomeMetodo];
-                    }  
-                }
-                return $data;
+            case 'get':                                    
+                return $this->dadosAtributos[$nomeMetodo];
+                break;            
+            case 'fix': // validacoes                
+                $dados = $dados[0];
+                switch($nomeMetodo){
+                    case 'notnull':
+                        if(empty($dados['vlr']) || $dados['vlr'] == "" || $dados['vlr'] == null){
+                            return false;
+                        }
+                        return true;
+                        break;
+                    default:
+                        throw new \Exception("O método $nomeMetodo de validação não existe", 720);        
+                        break;                
+                }               
                 break;
-        }       
+        }  
         
-    } 
+    }
     
     public function getValores(){
-        return $this->dados;
+        return $this->dadosAtributos;
     }
 
 }
--- Testes/ExamploUsuGenericaM.php
+++ Testes/ExamploUsuGenericaM.php
@@ -1,5 +1,5 @@
 
-
+<!--
 <link rel="stylesheet" type="text/css" href="View/css/cssTemporario.css"/> 
 <form action="enviar_email.php" method="post">
     <div class="agrupa-todo-mundo">
@@ -8,14 +8,51 @@
         <textarea placeholder="digite o conteudo do seu email" name="texto" required></textarea>
         <button type="submit">Enviar</button>
     </div>
+    
 </form>
-
+-->
 <?php
 
 
 // session_start();
-// require_once('Config/Define.php');
-// require_once(SITE_ROOT.DS.'autoload.php');  
+require_once('Config/Define.php');
+require_once(SITE_ROOT.DS.'autoload.php');  
+use Action\testeA;
+try{
+    $teste = new testeA();
+// $teste->getJaca('Daniel');
+// $teste->getJAqiunha('Churros');
+$teste->setJaca(
+    [
+        "vlr" => "Churros da Silva",
+        "validacoes" => [
+            "not null"
+        ]
+    ]
+    );    
+echo $teste->getJaca();
+
+$teste->setJui([
+    "vlr" => "suco de laranja",
+    "validacoes" => [
+        "not null"
+    ]
+]);
+echo '<br>';
+echo $teste->getJui();
+
+$teste->setJaquinha([
+    "vlr" => "Ola mundooo",
+    "validacoes" => [
+        "not null"
+    ]
+]);
+echo '<br>';
+echo $teste->getJaquinha();
+}catch(Exception $exc){
+    echo $exc->getMessage();
+
+}
 
 // echo '<form>
 //         <input type="email" name="email" placeholder="digite seu email">
