6237de64fbc77a75ef4d9a59bae60f13fac7a931
viniciuswz <vinicius-araujo0@hotmail.com>
2019-05-26 21:35:10 -0300

    parte de depoimentos ok

--- js/js.js
+++ js/js.js
@@ -3,6 +3,36 @@
 
 
 jQuery(function($){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+       
+        
+        responsive:{
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            1000:{
+                items:3
+            },
+            1200:{
+                items:3
+            },
+            1366:{
+                items:4
+            },
+            1666:{
+                items:5
+            },
+            1666:{
+                items:6
+            }
+            
+        }
+    })
     
     
     
