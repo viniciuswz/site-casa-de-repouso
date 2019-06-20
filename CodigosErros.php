f5a836f59d406c612df893122d09de28fb46fb89
Daniel Costa <danielcost0705@gmail.com>
2019-06-19 23:47:41 -0300

    Fazendo controle de erros

---
+++ CodigosErros.php
@@ -0,0 +1,14 @@
+<?php
+$erros = [
+    "10" => "Erro ao enviar a imagem -> TratarImg.class.php",
+    "12" => "Faltando informações -> ValidarCampos.class.php",
+    "312" => "Erro ao enviar email -> EmailMailer.class.php",
+    "720" => [
+        "É necessário setar os dados como Array para a propiedade",
+        "É necessário setar um valor na propiedade",
+        "É necessário setar as validacoes como Array para a propiedade",
+        "Os dados setados da propiedade não são válidos",
+        "O método de validação não existe",
+        "GenericaM.class.php"
+    ]
+];
